18a0a15a5ac76f60c0ffa5e26dab986a171d06de
r2rro <96370575+r2rro@users.noreply.github.com>
2022-02-09 11:13:50 -0800

    Update and rename main.f95 to Implicit FSC FDM Parabolic.f95

--- Implicit FSC FDM Parabolic.f95
+++ Implicit FSC FDM Parabolic.f95
@@ -1,6 +1,6 @@
         ! Implicit FTSC FDM Solver using TDMA algorithm and ADI method for parabolic 2D heat transfer equation
         !-------------------------------------------------------------
-        !------------------By Arthur Rostami -----------------
+        !------------------By Arthur Rostami -------------------------
         !-------------------------------------------------------------
 
 program CFD_FIRST_PROJECT_IMPLICIT
